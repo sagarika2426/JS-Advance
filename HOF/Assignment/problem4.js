7f8a10d88808aabf14a00a80b7816c77b616e13a
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2023-09-29 11:58:17 +0530

    ARRAY STRING FIRST CHARACTER - HOF

---
+++ HOF/Assignment/problem4.js
@@ -0,0 +1,14 @@
+// Given an array of string generate an array with their first characters
+
+// Sample Input - ["Masai", "School"]
+
+// Sample Output - ["M", "S"]
+
+var arr = ["Masai", "School"]
+var out = arr.map(function(el){
+  return el[0]
+})
+
+console.log(out)
+
+
