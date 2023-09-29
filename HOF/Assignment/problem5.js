c0e3d143b2f3a609e81e1baddbe5de47e04c7de2
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2023-09-29 11:58:57 +0530

    ARRAY STRING LENGTH - HOF

---
+++ HOF/Assignment/problem5.js
@@ -0,0 +1,9 @@
+// Given an array of strings print the length of each string
+
+var arr = ["My", "Name", "is", "Sagarika", "Sahoo"]
+
+var out = arr.map(function(el){
+  return el.length;
+})
+
+console.log(out)
