f63863ede9a61d9714ea018058c3beef980e1645
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2023-10-19 23:02:25 +0530

    classwork 2

---
+++ Prototype Inheritance/2.js
@@ -0,0 +1,10 @@
+// Nike product (brand, logo, seller) --- from flipkart
+
+const product = {
+  brand: "Nike",
+  logo: "url",
+  seller: "Flipkart",
+};
+
+let p1 = Object.create(product);
+console.log(p1.brand);
